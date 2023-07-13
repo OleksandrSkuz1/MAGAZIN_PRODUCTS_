3f5dd8cfab33d091e02e56fb3a3d2e7db38074b4
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-13 09:01:15 +0300

    add

---
+++ main.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+
+   # створюємо словник 'products' з назвою товарів та їх ціною
+
+    products = [{
+         'fruit': 'Апельсин',
+         'price': 'two',
+    },
+    {
+        'fruit': 'Банан',
+        'price': 'two',
+    },
+    {
+        'fruit': 'Ківі',
+        'price': 'two',
+    },
+    {
+        'fruit': 'Яблуко',
+        'price': 'two',
+    },
+    {
+        'fruit': 'Маракуя',
+        'price': 'two',
+    }
+    ]
+    print(products['fruit'])
